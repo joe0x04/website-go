17ef8b208be3d419b2b8c407902a14176417f640
Joe Reid <joe@joereid.com>
2021-02-13 15:02:54 -0500

    - trying out the gorilla mux library - playing around with random IDs, currently google's uuid

--- website.go
+++ website.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	log "fmt"
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"time"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 )
 
 type Todo struct {
@@ -17,14 +21,14 @@ type TodoPageData struct {
 	Todos     []Todo
 }
 
-func log_request(r *http.Request) {
+func logRequest(r *http.Request) {
 	t := time.Now()
-	log.Printf("%s %s %s %s\n", t.Format("20060102150405"), r.RemoteAddr, r.Method, r.URL.Path)
+	fmt.Printf("%s %s %s %s\n", t.Format("20060102150405"), r.RemoteAddr, r.Method, r.URL.Path)
 }
 
 // HandleTemplate...
 func HandleTemplate(w http.ResponseWriter, r *http.Request) {
-	log_request(r)
+	logRequest(r)
 	tmpl := template.Must(template.ParseFiles("login.gtmp"))
 	data := TodoPageData{
 		PageTitle: "My TODO list",
@@ -37,13 +41,38 @@ func HandleTemplate(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, data)
 }
 
-func main() {
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.HandleFunc("/", HandleTemplate)
-
-	log.Println("Listening on :8087")
-	http.ListenAndServe(":8087", nil)
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	logRequest(r)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
+func handleCommunity(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id := params["id"]
+	fmt.Fprintf(w, "<p>id: %s</p>\n", id)
+
+	randomID := uuid.New()
+	fmt.Fprintf(w, "<p>uuid: %s</p\n", randomID)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	// server static stuff from the /static path
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+	r.HandleFunc("/", HandleTemplate)
+	r.HandleFunc("/login", handleLogin)
+	r.HandleFunc("/community/{id}/home", handleCommunity)
+
+	log.Println("Listening on :8087")
+
+	server := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:8087",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	server.ListenAndServe()
+}
