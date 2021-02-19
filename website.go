07039777a71ed1376195af5f7873e991341ca0eb
Joe Reid <joe@joereid.com>
2021-02-19 09:28:26 -0500

    lots of little testing stuff - toml config file - sqlite db - nested templating

--- website.go
+++ website.go
@@ -1,15 +1,66 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/naoina/toml"
 )
+
+type TomlConfig struct {
+	HTTP struct {
+		Enabled   bool   `toml:"enabled"`
+		IPAddress string `toml:"ip"`
+		Port      int    `toml:"port"`
+	} `toml:"http"`
+
+	HTTPS struct {
+		Enabled   bool   `toml:"enabled"`
+		Port      int    `toml:"port"`
+		IPAddress string `toml:"ip"`
+		Cert      string `toml:"certificate"`
+		Key       string `toml:"privatekey"`
+	} `toml:"https"`
+
+	DB struct {
+		Type   string `toml:"type"`
+		File   string `toml:"file"`
+		Host   string `toml:"host"`
+		User   string `toml:"username"`
+		Pass   string `toml:"password"`
+		Schema string `toml:"schema"`
+	} `toml:"database"`
+}
+
+var config TomlConfig
+
+func load_config() {
+	var cfgfile string
+	if len(os.Args) == 2 {
+		cfgfile = os.Args[1]
+	} else {
+		cfgfile = "config.toml"
+	}
+
+	f, err := os.Open(cfgfile)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	if err := toml.NewDecoder(f).Decode(&config); err != nil {
+		panic(err)
+	}
+}
 
 type Todo struct {
 	Title string
@@ -17,6 +68,7 @@ type Todo struct {
 }
 
 type TodoPageData struct {
+	MainTitle string
 	PageTitle string
 	Todos     []Todo
 }
@@ -29,8 +81,23 @@ func logRequest(r *http.Request) {
 // HandleTemplate...
 func HandleTemplate(w http.ResponseWriter, r *http.Request) {
 	logRequest(r)
-	tmpl := template.Must(template.ParseFiles("login.gtmp"))
+
+	files := []string{
+		"template/home.html",
+		"template/header.html",
+		"template/footer.html",
+	}
+
+	tmpl, err := template.ParseFiles(files...)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
 	data := TodoPageData{
+		MainTitle: "Test title",
 		PageTitle: "My TODO list",
 		Todos: []Todo{
 			{Title: "Task 1", Done: false},
@@ -48,14 +115,37 @@ func handleLogin(w http.ResponseWriter, r *http.Request) {
 
 func handleCommunity(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	id := params["id"]
-	fmt.Fprintf(w, "<p>id: %s</p>\n", id)
+	id, err := strconv.ParseInt(params["id"], 10, 64)
+	fmt.Fprintf(w, "<p>id: %d</p>\n", id)
+
+	db, err := sql.Open("sqlite3", "./website.db")
+	if err != nil {
+		panic(err)
+	}
+
+	stmt, err := db.Prepare("SELECT name FROM stuff WHERE id = ? LIMIT 1")
+	defer stmt.Close()
+
+	row, err := stmt.Query(id)
+	if err != nil {
+		fmt.Printf("ERR: %s\n", err.Error())
+		return
+	}
+
+	defer row.Close()
+
+	for row.Next() {
+		var name string
+		row.Scan(&name)
+		fmt.Println(name)
+	}
 
 	randomID := uuid.New()
 	fmt.Fprintf(w, "<p>uuid: %s</p\n", randomID)
 }
 
 func main() {
+	load_config()
 	r := mux.NewRouter()
 
 	// server static stuff from the /static path
@@ -65,14 +155,13 @@ func main() {
 	r.HandleFunc("/login", handleLogin)
 	r.HandleFunc("/community/{id}/home", handleCommunity)
 
-	log.Println("Listening on :8087")
-
 	server := &http.Server{
 		Handler:      r,
-		Addr:         "127.0.0.1:8087",
+		Addr:         fmt.Sprintf("%s:%d", config.HTTP.IPAddress, config.HTTP.Port),
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 
+	log.Printf("Listening on %s (http)\n", server.Addr)
 	server.ListenAndServe()
 }
